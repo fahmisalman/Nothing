55bed2ac53b6b05e2f11d946aa7597ad1cbc0e97
fahmisalman <fahmisalman@gmail.com>
2019-03-02 09:05:11 +0700

    First commit

---
+++ Nothing/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Nothing
+//
+//  Created by Fahmi Salman Nurfikri on 2/24/19.
+//  Copyright © 2019 Fahmi Salman Nurfikri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
